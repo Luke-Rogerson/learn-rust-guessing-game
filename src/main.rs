1700e06433f6ed96a2a3f97a1db927d552c21a3d
Luke Rogerson <lukerogerson@aol.com>
2020-10-16 22:42:12 +0100

    Create guessing game

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+
+fn main() {
+    
+    let low = 1;
+    let high = 11;
+    let secret_number = rand::thread_rng().gen_range(low, high);
+    
+    println!("Guess the number between {} and {}!", low, high);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a bloody number");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Just right!");
+                break;
+            },
+        }
+    }
+}
